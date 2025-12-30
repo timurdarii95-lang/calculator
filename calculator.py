401b8962f92598dbe6d8c76db5253c5f3a0176c1
Timur Darii <timurdarii95@gmail.com>
2025-12-30 18:27:13 +0000

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+
+nr1 = int(input('Enter your first number: '))
+nr2 = int(input('Enter your second number: '))
+operation = input('Choose a operation: +, -, *, /: ')
+if operation == '+':
+    result = nr1+nr2
+elif operation =='-':
+    result = nr1-nr2
+elif operation == '*':
+    result = nr1*nr2
+elif operation == '/':
+    if nr2== 0:
+        result = 'ERROR'
+    else:
+        result = nr1/nr2
+else:
+    result = 'Invalid operation'
+print('Your result is: ',result)
